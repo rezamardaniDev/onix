00a9f8886e9b1e28cd80c264332429c2ab6d215d
Alireza <alirezasaadat056@gmail.com>
2024-10-22 18:28:53 +0330

    add: keyboards for user section

--- onix/utils/keyboards.php
+++ onix/utils/keyboards.php
@@ -1,6 +1,7 @@
 <?php
 
-// Main keyboard
+# -------------- Main keyboard -------------- #
+
 $mainKeyboard = json_encode([
     'resize_keyboard' => true,
     'keyboard' => [
@@ -12,10 +13,31 @@ $mainKeyboard = json_encode([
     ]
 ]);
 
-// keyboard for AI section 
-$AiKeyboard = json_encode([
+# -------------- keyboard for AI section -------------- #
+
+$aiKeyboard = json_encode([
     'resize_keyboard' => true,
     'keyboard' => [
         [['text' => 'GPT 3'] , ['text' => 'GPT 4.O']],
+    ]
+]);
+
+# -------------- Admin panel keyboard -------------- #
+ 
+$adminPanelKeyboard = json_encode([
+    'resize_keyboard' => true,
+    'keyboard' => [
+        [['text' => 'همگانی'] , ['text' => 'آمار']],
+        [['text' => 'کاربران']]
+    ]
+]);
+
+# -------------- User section keyboard -------------- #
+
+$userSectionKeyboard = json_encode([
+    'resize_keyboard' => true,
+    'keyboard' => [
+        [['text' => 'بلاک'] , ['text' => 'آنبلاک']],
+        [['text' => 'جست وجو'] , ['text' => 'افزودن موجودی']]
     ]
 ]);
