129e83993a2766bccd1662bb9fea724a3de9561e
Alireza <alirezasaadat056@gmail.com>
2024-10-27 21:03:03 +0330

    add: new module for translate section

--- onix/bot.php
+++ onix/bot.php
@@ -142,40 +142,10 @@ if ($text == '「 🎙 متن به ویس」' || $user->step == 'text-voice') {
 # -------------- translator button -------------- #
 
 if ($text == '「 🗣 مترجم متن 」') {
-    $bot->sendMessage($from_id, 'لطفا نوع ترجمه را انتخاب کنید: ', $translateKeyboard);
-    $userCursor->setStep($from_id, 'translator');
+    require 'modules/translator.php';
     die;
 }
 
-if ($user->step == 'translator') {
-    if ($text == '「 🇮🇷 مترجم انگلیسی به فارسی 」') {
-        $bot->sendMessage($from_id, 'لطفا متن انگلیسی خود را وارد کنید: ', $backButton);
-        $userCursor->setStep($from_id, 'translate-en-fa');
-        die;
-    }
-
-    if ($text == '「 🏴󠁧󠁢󠁥󠁮󠁧󠁿 مترجم فارسی به انگلیسی 」') {
-        $bot->sendMessage($from_id, 'لطفا متن فارسی خود را وارد کنید: ', $backButton);
-        $userCursor->setStep($from_id, 'translate-fa-en');
-        die;
-    }
-}
-
-if (preg_match('/^translate/', $user->step)) {
-    if ($user->step == 'translate-en-fa') {
-        $response = $apiRequest->translateToFa($text);
-        $bot->sendMessage($from_id, $response, $translateKeyboard);
-        $userCursor->setStep($from_id, 'translator');
-        die;
-    }
-
-    if ($user->step == 'translate-fa-en') {
-        $response = $apiRequest->translateToEn($text);
-        $bot->sendMessage($from_id, $response, $translateKeyboard);
-        $userCursor->setStep($from_id, 'translator');
-        die;
-    }
-}
 
 # -------------- weather section -------------- #
 
