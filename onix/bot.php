7ec0e72e379c3bd46946571c660b535e24aa3272
reza <mardanireza58@gmail.com>
2024-10-27 23:14:56 +0330

    add: new module and limits for soundCould download

--- onix/bot.php
+++ onix/bot.php
@@ -154,29 +154,7 @@ if ($text == '「 📥 دانلودر ها」') {
     die;
 }
 
-if ($text == '「 📻 دانلود ساندکلود 」') {
-    $bot->sendMessage($from_id, 'لینک ساندس کلود رو بفرست عشقم: ', $backButton);
-    $userCursor->setStep($from_id, 'get-sound-cloud');
-    die;
+if ($text == '「 📻 دانلود ساندکلود 」' || $user->step == 'get-sound-cloud') {
+    require 'modules/soundCouldDl.php';
 }
 
-if ($user->step == 'get-sound-cloud') {
-    $bot->sendMessage($from_id, '<b>درحال ارتباط با سرور ...</b>');
-    $response = $apiRequest->getSoundCloudId($text);
-    $soundInfo = $apiRequest->getSoundCloudInfo($response)->result;
-    $soundName = str_replace('.mp3', '', $soundInfo->title);
-
-    $bot->editMessage($chat_id, '<b>فایل مورد نظر یافت شد! درحال ارسال به تلگرام ...</b>', $message_id + 1);
-    $bot->sendChatAction($from_id, 'upload_document');
-
-    $getFile = $apiRequest->getSoundCloudFile($response);
-
-    $rand = rand(124588, 854963);
-    $audioFile = "SoundCloud-{$response}.mp3";
-
-    file_put_contents($audioFile, $getFile);
-    $bot->sendAudio($from_id, "https://fara-it.ir/onix/{$audioFile}", $soundName, $downloaderKeyboard);
-    unlink($audioFile);
-
-    die;
-}
---
+++ onix/modules/soundCouldDl.php
@@ -0,0 +1,31 @@
+<?php
+
+if ($text == '「 📻 دانلود ساندکلود 」'){
+    if ($userLimits->dl_soundcloud < 1) {
+        $bot->sendMessage($from_id, 'شما اعتبار کافی برای این بخش را ندارید.');
+        die;
+    }
+    $bot->sendMessage($from_id, 'لینک ساندس کلود رو بفرست عشقم: ', $backButton);
+    $userCursor->setStep($from_id, 'get-sound-cloud');
+    die;
+}else{
+    $bot->sendMessage($from_id, '<b>درحال ارتباط با سرور ...</b>');
+    $response = $apiRequest->getSoundCloudId($text);
+    $soundInfo = $apiRequest->getSoundCloudInfo($response)->result;
+    $soundName = str_replace('.mp3', '', $soundInfo->title);
+
+    $bot->editMessage($chat_id, '<b>فایل مورد نظر یافت شد! درحال ارسال به تلگرام ...</b>', $message_id + 1);
+    $bot->sendChatAction($from_id, 'upload_document');
+
+    $getFile = $apiRequest->getSoundCloudFile($response);
+
+    $rand = rand(124588, 854963);
+    $audioFile = "SoundCloud-{$response}.mp3";
+
+    file_put_contents($audioFile, $getFile);
+    $bot->sendAudio($from_id, "https://fara-it.ir/onix/{$audioFile}", $soundName, $downloaderKeyboard);
+    $userCursor->setLimit($from_id, 'dl_soundcloud', $userLimits->dl_soundcloud - 1);
+    unlink($audioFile);
+
+    die;
+}
