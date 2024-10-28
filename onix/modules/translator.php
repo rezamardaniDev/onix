b7f6b330dbec2c45da223753d404f3c3d07389d3
reza <mardanireza58@gmail.com>
2024-10-28 12:28:12 +0330

    debug: translator buttons

--- onix/modules/translator.php
+++ onix/modules/translator.php
@@ -1,26 +1,25 @@
 <?php
 
-if ($text == '「 🗣 مترجم متن 」') {
-    if ($text == '「 🗣 مترجم متن 」' || $user->step == 'translator') {
-        if ($text == '「 🗣 مترجم متن 」'){
-            $bot->sendMessage($from_id, 'لطفا نوع ترجمه را انتخاب کنید: ', $translateKeyboard);
-            $userCursor->setStep($from_id, 'translator');
-            die;
-        }
-        if ($text == '「 🇮🇷 مترجم انگلیسی به فارسی 」') {
-            $bot->sendMessage($from_id, 'لطفا متن انگلیسی خود را وارد کنید: ', $backButton);
-            $userCursor->setStep($from_id, 'translate-en-fa');
-            die;
-        }
-    
-        if ($text == '「 🏴󠁧󠁢󠁥󠁮󠁧󠁿 مترجم فارسی به انگلیسی 」') {
-            $bot->sendMessage($from_id, 'لطفا متن فارسی خود را وارد کنید: ', $backButton);
-            $userCursor->setStep($from_id, 'translate-fa-en');
-            die;
-        }
+if ($text == '「 🗣 مترجم متن 」' || $user->step == 'translator') {
+    if ($text == '「 🗣 مترجم متن 」') {
+        $bot->sendMessage($from_id, 'لطفا نوع ترجمه را انتخاب کنید: ', $translateKeyboard);
+        $userCursor->setStep($from_id, 'translator');
+        die;
+    }
+    if ($text == '「 🇮🇷 مترجم انگلیسی به فارسی 」') {
+        $bot->sendMessage($from_id, 'لطفا متن انگلیسی خود را وارد کنید: ', $backButton);
+        $userCursor->setStep($from_id, 'translate-en-fa');
+        die;
+    }
+
+    if ($text == '「 🏴󠁧󠁢󠁥󠁮󠁧󠁿 مترجم فارسی به انگلیسی 」') {
+        $bot->sendMessage($from_id, 'لطفا متن فارسی خود را وارد کنید: ', $backButton);
+        $userCursor->setStep($from_id, 'translate-fa-en');
+        die;
     }
     die;
 }
+
 
 if (preg_match('/^translate/', $user->step)) {
     if ($user->step == 'translate-en-fa') {
@@ -37,4 +36,3 @@ if (preg_match('/^translate/', $user->step)) {
         die;
     }
 }
-
