e31375c7bfef4f65df9534129c1651a1f363773a
reza <mardanireza58@gmail.com>
2024-10-29 01:25:40 +0330

    add: new keyboard for admin

--- onix/modules/startBot.php
+++ onix/modules/startBot.php
@@ -7,6 +7,7 @@ if (!$userLimits) {
     $userCursor->addNewUserLimits($from_id);
 }
 $botMessage = "سلام, به ربات هوش مصنوعی اونیکس خوش آمدید.\n\nجهت ادامه روی یکی از دکمه های زیر کلیک کنید.\n\n" . '<b>' . "ساخته شده توسط : " . '<a href="' . 'https://t.me/OnyxAiTeam' . '">*𝗢𝗻𝘆𝘅𝗧𝗲𝗮𝗺* 🦜</a>' . '</b>';
-$bot->sendMessage($chat_id, $botMessage, $mainKeyboard);
+$keyStart = $user->is_admin ? $mainKeyboard2 : $mainKeyboard1;
+$bot->sendMessage($chat_id, $botMessage, $keyStart);
 $userCursor->setStep($from_id, 'home');
 die;
