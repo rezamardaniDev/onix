ccd491a3cffa14dd92b43167a259f6c02bb7ed51
reza <mardanireza58@gmail.com>
2024-10-29 23:00:23 +0330

    update: when add bot to group

--- onix/partial/updateMessage.php
+++ onix/partial/updateMessage.php
@@ -2,6 +2,7 @@
 
 
 if ($bot_join == 'onixToolsBot') {
+
     if (!$group) {
         $groupCursor->addNewGroup($chat_id);
     }
@@ -10,6 +11,7 @@ if ($bot_join == 'onixToolsBot') {
 
 • تا فرآیند نصب و پیکربندی انجام شود";
     $bot->sendMessage($chat_id, $botMessage);
+    die;
 }
 
 # -------------- send message when is admin -------------- #
@@ -18,40 +20,25 @@ if ($bot_admin == "onixToolsBot" && $bot_status == "administrator") {
     if (!$group) {
         $groupCursor->addNewGroup($chat_id);
     }
+
     $groupCursor->setActive($chat_id, 1);
     $botMessage = "
-    <b>✅ اونیکس 🦜, با موفقیت در گروه نصب شد!
+    *✅ اونیکس 🦜, با موفقیت در گروه نصب شد!
     
     برای دسترسی به بخش دستورات و قابلیت های ربات و همچنین اموزش کار با اونیکس بر روی دکمه (LAUNCH) کلیک کنید.\n\n
     
-    🤖 بخشی از قابلیت های ربات:
+    🤖 بخشی از قابلیت های ربات:*
+    ```
     چت با هوش مصنوعی ChatGpt 3.5 Turbo
     ارز های دیجیتال 
     اوقات شرعی 
-    اخبار و...</b>";
-    $bot->sendMessage($chat_id, $botMessage);
+    اخبار و...
+    ```";
+
+    $bot->sendMessage($chat_id, $botMessage, mrk: 'Markdown');
     $groupCursor->setActive($chat_id, 1);
-}
-
-# -------------- send message when added to group but not admin -------------- #
-
-if ($type == "supergroup" && $group->is_active != 1) {
-    $botMessage = "
-• ربات را ادمین کامل گروه نمایید
-
-• تا بتوانید از قابلیت های ربات استفاده نمایید";
-    $bot->sendMessage($chat_id, $botMessage);
     die;
 }
-
-# -------------- change is_active to 0 when banned -------------- #
-
-
-if ($bot_admin == "onixToolsBot" && ($bot_status == "kicked" || $bot_status == "member")) {
-    $groupCursor->setActive($chat_id, 0);
-}
-
-
 
 # -------------- first comment in group -------------- #
 
@@ -63,5 +50,3 @@ if ($update->message->from->first_name == 'Telegram') {
     ";
     $bot->sendMessage($update->message->chat->id, $botMessage, message_id: $message_id);
 }
-
-
