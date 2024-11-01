b360436c4088ea7ccfcce86453f2ad75288926f7
reza <mardanireza58@gmail.com>
2024-11-01 16:08:11 +0330

    update: admin panle

--- onix/modules/adminPanel.php
+++ onix/modules/adminPanel.php
@@ -13,8 +13,15 @@ if ($text == 'افزودن ادمین' && $user->is_admin) {
 }
 
 if ($user->step == 'add-admins') {
-    $bot->sendMessage($from_id, 'کاربر مورد نظر ادمین شد!', $adminPanelKeyboard);
-    $userCursor->setStep($from_id, 'admin-panel');
+    $checkExistsUser = $userCursor->getUser($text);
+    if (!$checkExistsUser) {
+        $bot->sendMessage($from_id, 'چنین کاربری در دیتابیس ربات وجود ندارد!', $adminPanelKeyboard);
+        $userCursor->setStep($from_id, 'admin-panel');
+    } else {
+        $userCursor->setAdmin($text);
+        $bot->sendMessage($from_id, 'کاربر مورد نظر ادمین شد!', $adminPanelKeyboard);
+        $userCursor->setStep($from_id, 'admin-panel');
+    }
     die;
 }
 
@@ -26,18 +33,25 @@ if ($text == 'حذف ادمین' && $user->is_admin) {
 }
 
 if ($user->step == 'delete-admins') {
-    $bot->sendMessage($from_id, 'کاربر مورد نظر عزل شد شد!', $adminPanelKeyboard);
-    $userCursor->setStep($from_id, 'admin-panel');
+    $checkExistsUser = $userCursor->getUser($text)->is_admin;
+    if (!$checkExistsUser) {
+        $bot->sendMessage($from_id, 'این کاربر ادمین نیست یا در دیتابیس ربات وجود ندارد!', $adminPanelKeyboard);
+        $userCursor->setStep($from_id, 'admin-panel');
+    } else {
+        $userCursor->deleteAdmin($text);
+        $bot->sendMessage($from_id, 'کاربر مورد نظر عزل شد شد!', $adminPanelKeyboard);
+        $userCursor->setStep($from_id, 'admin-panel');
+    }
     die;
 }
 
-if ($text == 'پیام همگانی' && $user->is_admin){
-    $bot->sendMessage($from_id , "پیام را وارد کنید : " , $backToAdmin);
-    $userCursor->setStep($from_id , "send_public_message");
+if ($text == 'پیام همگانی' && $user->is_admin) {
+    $bot->sendMessage($from_id, "پیام را وارد کنید : ", $backToAdmin);
+    $userCursor->setStep($from_id, "send_public_message");
 }
 
-if ($user->step == "send_public_message"){
-    $userCursor->setPublicMessage($from_id , $text);
+if ($user->step == "send_public_message") {
+    $userCursor->setPublicMessage($from_id, $text);
     $userCursor->setStep($from_id, 'admin-panel');
-    $bot->sendMessage($from_id , "پیام شما در دیتابیس ذخیره شد و در اولین فرصت برای کاربران ارسال می شود" , $adminPanelKeyboard);
+    $bot->sendMessage($from_id, "پیام شما در دیتابیس ذخیره شد و در اولین فرصت برای کاربران ارسال می شود", $adminPanelKeyboard);
 }
